895741da0f5b267b899a6cdb04d78467be4d4523
okunsimon3 <111754449+okunsimon3@users.noreply.github.com>
2025-04-23 11:46:26 -0400

    homepage adding

---
+++ src/Home.jsx
@@ -0,0 +1,38 @@
+import { useRef } from "react";
+import { NavLink } from "react-router-dom"
+import './Home.css'
+import reel from './assets/Copacetic_Logo_Reel.mov'
+
+export default function Home() {
+    const videoRef = useRef(null);
+
+    const handleMouseEnter = () => {
+        videoRef.current && videoRef.current.pause();
+    };
+
+    const handleMouseLeave = () => {
+        videoRef.current && videoRef.current.play();
+    };
+
+    return (
+        <>
+        <div className="home-container">
+            <video
+                ref={videoRef}
+                className="logo-reel"
+                src={reel}
+                autoPlay
+                muted
+                loop
+            ></video>
+            <h3>Hello</h3>
+            <NavLink 
+                className="home-link"
+                to="./media"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >Enter Site</NavLink>
+        </div>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from '../Home.jsx'
+import Home from './Home.jsx'
 import Media from './Media.jsx'
 
 createRoot(document.getElementById('root')).render(
