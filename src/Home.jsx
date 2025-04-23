c8e3448934e4d3f038d58a42a26a35d53fe855e6
okunsimon3 <111754449+okunsimon3@users.noreply.github.com>
2025-04-23 11:59:51 -0400

    more home changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,28 +1,29 @@
 import { useRef } from "react";
 import { NavLink } from "react-router-dom"
 import './Home.css'
-// import reel from './assets/Copacetic_Logo_Reel.mov'
+import reel from './assets/Copacetic_Logo_Reel.mov'
 
 export default function Home() {
-    // const videoRef = useRef(null);
+    const videoRef = useRef(null);
 
-    // const handleMouseEnter = () => {
-    //     videoRef.current && videoRef.current.pause();
-    // };
+    const handleMouseEnter = () => {
+        videoRef.current && videoRef.current.pause();
+    };
 
-    // const handleMouseLeave = () => {
-    //     videoRef.current && videoRef.current.play();
-    // };
+    const handleMouseLeave = () => {
+        videoRef.current && videoRef.current.play();
+    };
 
     return (
         <>
         <div className="home-container">
+            <video src={reel} autoPlay muted></video>
             <h3>Hello</h3>
             <NavLink 
                 className="home-link"
                 to="./media"
-                // onMouseEnter={handleMouseEnter}
-                // onMouseLeave={handleMouseLeave}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
             >Enter Site</NavLink>
         </div>
         </>
