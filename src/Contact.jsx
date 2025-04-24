10ce943bf1a0530386d77c72f0b678994820f246
okunsimon3 <111754449+okunsimon3@users.noreply.github.com>
2025-04-24 02:32:43 -0400

    final try

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,12 +1,36 @@
 import './components/Contact.css'
 
 const Contact = () => (
-    <div>
-        <form name="contact" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <input type="text" name="name" />
-        <button type="submit">Send</button>
-        </form>
+    <div className="contact-page">
+        <div className='contact-container'>
+            <div className='contact-left'>
+                <h1>Get In Touch</h1>
+                <p>Hello</p>
+            </div>
+            <form className='contact-form' 
+            name='contact'
+            method='POST'
+            data-netlify='True'
+            >
+                <input type="hidden" name="form-name" value="contact" />
+                <div>
+                    <label>First Name
+                        <input type="text" name="first-name" />
+                    </label>
+                </div>
+                <div>
+                    <label>Email
+                        <input type="email" name="email" />
+                    </label>
+                </div>
+                <div>
+                    <label>More Info
+                        <textarea name="message"></textarea>
+                    </label>
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     </div>
 );
 
